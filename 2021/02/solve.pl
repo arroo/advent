3ab7bb455bf4bff9f76e79efdbb8e470ea780ffb
Mozes Magyar <mozes.magyar@indexexchange.com>
2021-12-02 00:28:47 -0500

    using reduce

--- 2021/02/solve.pl
+++ 2021/02/solve.pl
@@ -3,56 +3,77 @@
 use warnings;
 use strict;
 
-use AOC::Utils qw(slurp reduce);
+use AOC::Utils qw(slurp reduceFn);
 
 use Data::Dumper;
 
 sub solveOne {
-	my ($input) = @_;
+	my ($lines) = @_;
 
-	my $horizontal = 0;
-	my $depth = 0;
+	my $status = reduceFn(
+		sub {
+			my ($line) = @_;
 
-	for my $line (@$input) {
-		my ($direction, $amount) = $line =~ m/(.+) (\d+)/;
+			return $line =~ m/(.+) (\d+)/;
+		},
+		sub {
+			my ($acc, $direction, $amount) = @_;
 
-		if ($direction eq 'forward') {
-			$horizontal += $amount;
-		} elsif ($direction eq 'down') {
-			$depth += $amount;
-		} elsif ($direction eq 'up') {
-			$depth -= $amount;
-		} else {
-			print "unknown direction for line '$line'\n";
-		}
-	}
 
-	return $horizontal * $depth;
+			my ($horizontal, $depth) = @$acc;
+
+			if ($direction eq 'forward') {
+				$horizontal += $amount;
+			} elsif ($direction eq 'down') {
+				$depth += $amount;
+			} elsif ($direction eq 'up') {
+				$depth -= $amount;
+			} else {
+				print "unknown direction $direction\n";
+			}
+
+			return [$horizontal, $depth];
+		},
+		$lines,
+		[0, 0],
+	);
+
+	return $status->[0] * $status->[1];
 }
 
 sub solveTwo {
-	my ($input) = @_;
+	my ($lines) = @_;
 
-	my $horizontal = 0;
-	my $depth = 0;
-	my $aim = 0;
+	my $status = reduceFn(
+		sub {
+			my ($line) = @_;
 
-	for my $line (@$input) {
-		my ($direction, $amount) = $line =~ m/(.+) (\d+)/;
+			return $line =~ m/(.+) (\d+)/;
+		},
+		sub {
+			my ($acc, $direction, $amount) = @_;
 
-		if ($direction eq 'forward') {
-			$horizontal += $amount;
-			$depth += $aim * $amount;
-		} elsif ($direction eq 'down') {
-			$aim += $amount;
-		} elsif ($direction eq 'up') {
-			$aim -= $amount;
-		} else {
-			print "unknown direction for line '$line'\n";
-		}
-	}
+			my ($horizontal, $depth, $aim) = @$acc;
 
-	return $horizontal * $depth;
+			if ($direction eq 'forward') {
+				$horizontal += $amount;
+				$depth += $aim * $amount;
+			} elsif ($direction eq 'down') {
+				$aim += $amount;
+			} elsif ($direction eq 'up') {
+				$aim -= $amount;
+			} else {
+				print "unknown direction $direction\n";
+			}
+
+			return [$horizontal, $depth, $aim];
+		},
+		$lines,
+		[0, 0, 0],
+	);
+
+	return $status->[0] * $status->[1];
+
 }
 
 sub main {
