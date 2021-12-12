193b658c7aa96b5f6f71eb2ac518b4f3367ff6c3
Mozes Magyar <mozes.magyar@indexexchange.com>
2021-12-11 23:36:50 -0500

    visualization!

--- 2021/11/solve.pl
+++ 2021/11/solve.pl
@@ -8,6 +8,7 @@ use AOC::Math qw(:all);
 use AOC::Utils qw(:all);
 
 use Data::Dumper;
+use Time::HiRes qw(usleep);
 
 sub sweep {
 	my ($field, $fn) = @_;
@@ -39,6 +40,37 @@ sub neighbours {
 	return \@neighs;
 }
 
+sub draw {
+	my ($field) = @_;
+
+	my $str = "\033[2J\033[0;0H";
+
+	#print "\033[2J";   #clear the screen
+	#print "\033[0;0H"; #jump to 0,0
+
+	for my $row (0 .. $#$field) {
+		for my $col (0 .. $#{$field->[$row]}) {
+			my $n = $field->[$row][$col];
+
+			$str .= "\e[1m" if ($n == 0);
+			$str .= "$n";
+			$str .= "\e[0m" if ($n == 0);
+
+			next;
+
+			print "\e[1m" if ($n == 0);
+			print "$n";
+			print "\e[0m" if ($n == 0);
+		}
+
+		$str .= "\n";
+		#print "\n";
+	}
+
+	$str =~ s/\e\[0m\e\[1m//g;
+	print "$str";
+}
+
 my $max = 10;
 
 sub makeSweep {
@@ -66,31 +98,16 @@ sub solveOne {
 		my @flashes;
 		my $sweepFn = makeSweep(\@flashes);
 		# First, the energy level of each octopus increases by 1.
-		# Then, any octopus with an energy level greater than 9 flashes.
 		sweep(
 			\@field,
 			$sweepFn,
 		);
 
-		# This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. If this causes an octopus to have an energy level greater than 9, it also flashes. This process continues as long as new octopuses keep having their energy level increased beyond 9. (An octopus can only flash at most once per step.)
+		# Then, any octopus with an energy level greater than 9 flashes. This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. If this causes an octopus to have an energy level greater than 9, it also flashes. This process continues as long as new octopuses keep having their energy level increased beyond 9. (An octopus can only flash at most once per step.)
 		while (scalar @flashes) {
-			my ($row, $col) = @{pop @flashes};
-
 			$totalFlashes++;
 
-			my $neighs = neighbours(\@field, $row, $col);
-
-
-
-			for my $coords (@$neighs) {
-				my ($row, $col) = @$coords;
-
-				$field[$row][$col]++;
-
-				if ($field[$row][$col] == $max) {
-					push @flashes, [$row, $col];
-				}
-			}
+			$sweepFn->(\@field, @$_) for @{neighbours(\@field, @{pop @flashes})};
 		}
 
 		# Finally, any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash.
@@ -117,46 +134,23 @@ sub solveTwo {
 	my $totalFlashes = 0;
 
 	for (my $i = 1; ;$i++) {
+
 		my @flashes;
-		# add 1
+		my $sweepFn = makeSweep(\@flashes);
+		# First, the energy level of each octopus increases by 1.
 		sweep(
 			\@field,
-			sub {
-				my ($field, $row, $col) = @_;
-
-				$field->[$row][$col]++;
-
-				if ($field->[$row][$col] == $max) {
-					$totalFlashes++;
-					push @flashes, [$row, $col];
-				}
-
-				return;
-			},
+			$sweepFn,
 		);
 
+		# Then, any octopus with an energy level greater than 9 flashes. This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. If this causes an octopus to have an energy level greater than 9, it also flashes. This process continues as long as new octopuses keep having their energy level increased beyond 9. (An octopus can only flash at most once per step.)
 		while (scalar @flashes) {
-			my ($row, $col) = @{pop @flashes};
+			$totalFlashes++;
 
-			#print "flash $row $col\n";
-
-			my $neighs = neighbours(\@field, $row, $col);
-
-			for my $coords (@$neighs) {
-				my ($row, $col) = @$coords;
-
-				#print "\tneigh $row $col\n";
-
-				$field[$row][$col]++;
-
-				if ($field[$row][$col] == $max) {
-					$totalFlashes++;
-					push @flashes, [$row, $col];
-				}
-			}
+			$sweepFn->(\@field, @$_) for @{neighbours(\@field, @{pop @flashes})};
 		}
 
-		# reset to 0 all flashes
+		# Finally, any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash.
 		# check if all flashed this round
 		my $notFlashed = 0;
 		sweep(
@@ -171,6 +165,11 @@ sub solveTwo {
 				}
 			},
 		);
+
+		draw(\@field);
+		print "$i\n$totalFlashes\n";
+		# 1 millisecond == 1000 microseconds
+		usleep(250_000);
 
 		if (not $notFlashed) {
 			return $i;
